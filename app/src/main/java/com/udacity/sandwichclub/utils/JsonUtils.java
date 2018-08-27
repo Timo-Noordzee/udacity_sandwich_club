ce08555698c5ec16569e2571c0e241d8ba244222
Timo Noordzee <timo_noordzee@hotmail.com>
2018-08-27 12:41:38 +0200

    Design the layout for the detail activity so the different elements display in a sensible way. Implement the JSON parsing in JsonUtils so it produces a Sandwich Object that can be used to populate the UI that you designed.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,62 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String JSON_NAME_OBJECT_KEY = "name";
+    private static final String JSON_NAME_MAIN_KEY = "mainName";
+    private static final String JSON_NAME_ALSO_KNOWN_AS_KEY = "alsoKnownAs";
+    private static final String JSON_ORIGIN_KEY = "placeOfOrigin";
+    private static final String JSON_DESCRIPTION_KEY = "description";
+    private static final String JSON_IMAGE_KEY = "image";
+    private static final String JSON_INGREDIENTS_KEY = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
+
+        String mainName;
+        List<String> alsoKnownAs = null;
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients = null;
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject nameObject = jsonObject.getJSONObject(JSON_NAME_OBJECT_KEY);
+
+            mainName = nameObject.getString(JSON_NAME_MAIN_KEY);
+            alsoKnownAs = getStringArrayData(nameObject.getJSONArray(JSON_NAME_ALSO_KNOWN_AS_KEY));
+            placeOfOrigin = jsonObject.getString(JSON_ORIGIN_KEY);
+            description = jsonObject.getString(JSON_DESCRIPTION_KEY);
+            image = jsonObject.getString(JSON_IMAGE_KEY);
+            ingredients = getStringArrayData(jsonObject.getJSONArray(JSON_INGREDIENTS_KEY));
+
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+            return sandwich;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
+    }
+
+    private static List<String> getStringArrayData(JSONArray jsonArray) throws JSONException{
+        List<String> data = new ArrayList<String>();
+        for(int i = 0; i < jsonArray.length(); i++){
+            Log.d("Sandwich", jsonArray.getString(i));
+            data.add(jsonArray.getString(i));
+        }
+        return data;
     }
 }
